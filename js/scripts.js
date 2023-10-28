43f714ff40095aee7dd1257e805bd2654a0b3a2c
pavendan01 <patricia.avendano1@gmail.com>
2023-10-28 16:16:15 -0300

    Validación del formulario

--- js/scripts.js
+++ js/scripts.js
@@ -28,3 +28,47 @@ function restartSlider() {
 
 // Llamar a la función para reiniciar el slider
 restartSlider();
+
+// Función que Valida el formulario
+
+document.getElementById('Formulario').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita que el formulario se envíe automáticamente
+    validarFormulario();
+});
+
+function validarFormulario() {
+  // Obtener los valores de los campos del formulario
+  var nombre = document.getElementById("nombre").value;
+  var apellido = document.getElementById("apellido").value;
+  var email = document.getElementById("email").value;
+  
+  // Expresión regular para validar que solo contenga letras
+  var valLetras = /^[A-Za-z]+$/;
+  
+  // Expresión regular para validar el formato de correo electrónico
+  var valEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  
+  // Verificar si los campos nombre y apellido están vacíos o no cumplen con las validaciones
+  if (nombre === "" || !valLetras.test(nombre)) {
+    alert("Por favor, ingrese un nombre válido.");
+    document.getElementById('nombre').focus();
+    return false;
+  }
+  
+  if (apellido === "" || !valLetras.test(apellido)) {
+    alert("Por favor, ingrese un apellido válido.");
+    document.getElementById('apellido').focus();
+    return false;
+  }
+  
+  //Verificar si la estructura del email introducido es correcto
+  if (email === "" || !valEmail.test(email)) {
+    alert("Por favor, ingrese un correo electrónico válido.");
+    document.getElementById('email').focus();
+    return false;
+  }
+  
+  // Mostrar mensaje de validación exitosa
+  alert("Gracias por dejarnos tus comentarios.");
+
+}
