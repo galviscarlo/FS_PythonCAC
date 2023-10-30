cedb79bdb83dd0da2bd37afbbb837fa4e8334390
pavendan01 <patricia.avendano1@gmail.com>
2023-10-30 01:15:52 -0300

    Inclusión de API

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,41 @@
+//Uso de la API
+ 
+fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+// Exito
+    .then(response => response.json())  // convertir a json
+    .then(json => console.log(json))    //imprimir los datos en la consola
+    .catch(err => console.log('Solicitud fallida: ', err)); // Capturar errores
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+    async function logRandomFru() {
+        const response = await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
+
+        const receta = await response.json();
+        console.log(receta);
+
+        document.querySelector("#Receta #Meal").textContent = receta.meals[0].strMeal;
+        document.querySelector("#Receta #Category").textContent = receta.meals[0].strCategory;
+        document.querySelector("#Receta #Picture").src = receta.meals[0].strMealThumb;
+
+/* 
+console.log(receta.meals);                     
+console.log(receta.meals[0].strMeal);
+console.log(receta.meals[0].strCategory);
+console.log(receta.meals[0].strMealThumb);
+console.log(user.results[0].name.last);
+console.log(user.results[0].picture.thumbnail);
+
+*/
+}
+
+logRandomFru();
+
 // Función que Valida el formulario
 
-document.getElementById('Formulario').addEventListener('submit', function(event) {
+/*document.getElementById('Formulario').addEventListener('submit', function(event) {
     event.preventDefault(); // Evita que el formulario se envíe automáticamente
     validarFormulario();
-});
+});*/
 
 function validarFormulario() {
   // Obtener los valores de los campos del formulario
